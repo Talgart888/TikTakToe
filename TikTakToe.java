24629e6aa9cea978b8d1632949ca48d682bc2537
Talgart888 <talgart2030@gmail.com>
2022-12-10 12:41:52 +0600

    Prepare the frame according to the task

---
+++ TikTakToe.java
@@ -0,0 +1,50 @@
+package JavaCore.TikTakToe;
+
+import java.util.Random;
+
+public class TikTakToe {
+    public static void main(String[] args) {
+        System.out.println("Используйте следующие комбинации, чтобы указать ячейку, используя числа от 1 до 9.");
+        printTableMapping(); // вывод таблицу соответсвия
+
+        /* определяем кто ходит первым: компьютер или пользователь через Random и через условия boolean,
+           если true то ходит компьютер т.к. внутри заложен его функция, если false то условия закрывается и идем к while
+           где ходит пользователь
+        */
+        if (new Random().nextBoolean()) {
+            makeComputerMove(); // компьютер делает ход
+            printGameTable(); // текущее состояние игрового поля
+        }
+
+        // Если никто не выиграл, то игра продолжается через цикл while
+        while (true) {
+            makeUserMove(); // пользователь делает ход
+            printGameTable(); // текущее состояние игрового поля
+
+            // если пользователь выиграл:
+            if (isUserWin()) {
+                System.out.println("ВЫ ВЫИГРАЛИ!");
+                break;
+            }
+            // Если нечья:
+            if (isDraw()) {
+                System.out.println("Извините, ничья.");
+                break;
+            }
+            MakeComputerMove(); // компьютер делает ход
+            printGameTable(); // текущее состояние игрового поля
+
+            // если компьютер выиграл
+            if (isComputerWin()) {
+                System.out.println("Компьютер выиграл(");
+                break;
+            }
+            // Если нечья:
+            if (isDraw()) {
+                System.out.println("Извините, ничья.");
+                break;
+            }
+        }
+        System.out.println("Игра завершена.");
+    }
+}
