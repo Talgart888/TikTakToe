cdfb062b3f8436cb62ab2183de40212effe25dff
Talgart888 <talgart2030@gmail.com>
2022-12-12 01:16:58 +0600

    Implementing the MakeComputerMove Function

--- TikTakToe.java
+++ TikTakToe.java
@@ -62,6 +62,7 @@ public class TikTakToe {
         };
         printGameTable(mappingTable);
     }
+
     // текущее состояние игрового поля
     private static void printGameTable(char[][] gameTable) {
         for (int i = 0; i < 3; i++) {
@@ -82,8 +83,19 @@ public class TikTakToe {
 
     }
 
+    // компьютер делает ход, заполняет пустые ячейки printGameTable ноликом '0'
     private static void MakeComputerMove(char[][] gameTable) {
-
+        Random random = new Random();
+        // через while компьютер будет заполнять пустые ячейки до тех пор пока не будет полностью заполнена
+        while (true) {
+            int row = random.nextInt(3); // bound 3 - возвращает случайное число в строку от 0 до 2
+            int col = random.nextInt(3); // bound 3 - возвращает случайное число в столбцу от 0 до 2
+            // если игровое поле по индексу row, col пуста, то в эту ячейку записываем 0
+            if (gameTable[row][col] == ' ') {
+                gameTable[row][col] = '0';
+                return;
+            }
+        }
     }
 
     private static boolean isUserWin(char[][] gameTable) {
